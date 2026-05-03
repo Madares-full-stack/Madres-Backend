3b04d50b3d8d66b673bbe7f37b519550864389e8
Issa Abu Hadhoud <107874322+IssaHadhoud@users.noreply.github.com>
2026-05-03 19:15:27 +0300

    Delete models/db.js

--- models/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/madares");
-  console.log("DB Ready To Use");
-};
-
-module.exports = connectDB;
